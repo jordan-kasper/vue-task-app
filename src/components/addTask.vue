4cf615f74c7eb239a018f2823129c81fa937b6f8
Jordan Kasper <jordan.kasper@gmail.com>
2019-12-13 12:19:43 -0800

    adding validation to new task form

--- src/components/addTask.vue
+++ src/components/addTask.vue
@@ -1,28 +1,27 @@
 <template>
   <form @submit.prevent="submitTask">
     <div>
-      <div>
-        <input
-          type="text"
-          class="form-control spacing"
-          v-model="newTask.taskValue"
-          placeholder="Add a Task"
-        />
-      </div>
-      <div>
-        <select
-          class="custom-select d-block w-100 spacing"
-          v-model="newTask.taskPriority"
-        >
-          <option selected>Choose...</option>
-          <option value="list-group-item-danger">Life Changing</option>
-          <option value="list-group-item-warning">Important</option>
-          <option value="list-group-item-success">Meh</option>
-        </select>
-      </div>
-      <div>
-        <button class="btn btn-primary btn-block spacing">Add Task</button>
-      </div>
+      <input
+        required
+        type="text"
+        name="task-name"
+        class="form-control spacing"
+        v-model="newTask.taskValue"
+        placeholder="Add a Task"
+      />
+    </div>
+    <select
+      required
+      class="custom-select d-block w-100 spacing"
+      v-model="newTask.taskPriority"
+    >
+      <option selected>Choose...</option>
+      <option value="list-group-item-danger">Life Changing</option>
+      <option value="list-group-item-warning">Important</option>
+      <option value="list-group-item-success">Meh</option>
+    </select>
+    <div>
+      <button class="btn btn-primary btn-block spacing">Add Task</button>
     </div>
   </form>
 </template>
@@ -39,8 +38,8 @@ export default {
   },
   methods: {
     /**
-      * Sends newly created task back to the parent component to be added to the tasks array
-      */
+     * Sends newly created task back to the parent component to be added to the tasks array
+     */
     submitTask() {
       this.$emit('taskAdded', this.newTask);
       this.newTask = {};
