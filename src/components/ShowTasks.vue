2de372ea23c1ca2a0c01075857c299711f7bdbd4
Jordan Kasper <jordan.kasper@gmail.com>
2020-02-26 08:04:17 -0800

    adding priority editing functionality

--- src/components/ShowTasks.vue
+++ src/components/ShowTasks.vue
@@ -9,11 +9,26 @@
       </thead>
       <tbody>
         <tr v-for="task in sortedTasks" v-bind:key="task">
-          <td><input contenteditable v-model=task.value
-          @blur="editTask(task)" ></td>
-          <td v-if="task.priority === '1'">Life Changing</td>
-          <td v-if="task.priority === '2'">Important</td>
-          <td v-if="task.priority === '3'">Meh</td>
+          <td>
+            <input
+              contenteditable
+              v-model="task.value"
+              @blur="editTask(task)"
+            />
+          </td>
+
+          <td>
+            <select class="custom-select d-block w-100 spacing">
+              <option selected v-if="task.priority === '1'"
+                >Life Changing</option
+              >
+              <option selected v-if="task.priority === '2'">Important</option>
+              <option selected v-if="task.priority === '3'">Meh</option>
+              <option value="1">Life Changing</option>
+              <option value="2">Important</option>
+              <option value="3">Meh</option>
+            </select>
+          </td>
           <td><a @click.prevent="deleteTask(task)">Delete</a></td>
         </tr>
       </tbody>
@@ -69,10 +84,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 input {
-      border-top-style: hidden;
-      border-right-style: hidden;
-      border-left-style: hidden;
-      border-bottom-style: hidden;
-      background-color: #eee;
-      }
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+  background-color: #eee;
+}
+select {
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+  padding-left: 0;
+  background-color: #eee;
+}
 </style>
